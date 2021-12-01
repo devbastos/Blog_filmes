c7ca115db304e7cea2080278c08cc4abd7fe1ebd
Gabriel Bastos <gbastosbarbosa@gmail.com>
2021-12-01 20:11:05 -0300

    Banco de Dados

---
+++ assets/Banco de dados/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'Cadasto';
+
+$conexao = new mysqli ($dbHost,$dbUsername,$dbPassword,$dbName)
+
+//if($conexao -> conect_errno)
+//{
+//    echo "erro";
+//}
+//else {
+//    echo "conexão efetuado com sucesso"
+//}
+?>
